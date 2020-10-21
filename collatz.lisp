cc5347e059c57a18c01ea3f2c7032f10a1fc20c7
Christopher Hoover <ch@murgatroid.com>
2020-10-20 21:02:55 -0700

    Allows for compilation without I/O

--- collatz.lisp
+++ collatz.lisp
@@ -4,8 +4,13 @@
 
 (defconstant limit (expt 10 1000))
 
+(defparameter *noisy* t)
+
+(defmacro noisy (&rest form)
+  (if *noisy* `(progn ,@form)))
+
 (defun collatz (a)
-  (declare (type integer a))
+  (declare (type (integer 0) a))
   (let ((seen (make-hash-table :test 'eql))
 	(p nil))
     (loop
@@ -17,23 +22,24 @@
 	      (setf p "odd")))
        (cond ((= a 1)
 	      (let ((len (hash-table-size seen)))
-		(format t "Finished in ~d moves~%" len)
+		(noisy (format t "Finished in ~d moves~%" len))
 		(return len)))
 	     ((gethash a seen)
-	      (format t "Counterexample! ~d~%" a)
+	      (noisy (format t "Counterexample! ~d~%" a))
 	      (throw 'counterexample a)))
        (setf (gethash a seen) t)
-       (format t "~d ~s ~d~%" a p (hash-table-size seen)))))
+       (noisy (format t "~d ~s ~d~%" a p (hash-table-size seen))))))
 
 (defun trial (random-state)
   (let ((a (random limit random-state)))
-    (format t "========== ~s a: ~d~%" random-state a)
+    (noisy (format t "========== ~s a: ~d~%" random-state a))
     (collatz a)
-    (format t "==========~%")))
+    (noisy (format t "==========~%"))))
 
 (defun run ()
   ;; handling of random state is not deterministic like strick's code
   (do ((n 0 (+ n 1))
        (random-state (make-random-state t) (make-random-state random-state)))
       ((= n 1000000))
+    (declare (type fixnum n))
     (trial random-state)))
